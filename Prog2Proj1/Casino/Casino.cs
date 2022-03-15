f5dcae853ae5ea28c09e90936b49882707b0a0e1
VikingHesse <70264379+VikingHesse@users.noreply.github.com>
2022-03-15 13:32:41 +0100

    Add files via upload

---
+++ Prog2Proj1/Casino/Casino.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Casino
+    {
+        public List<Employees> Employees { get; set; } = new List<Employees>();
+
+        public List<User> User { get; set; } = new List<User>();
+
+    }
+}
---
+++ Prog2Proj1/Casino/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            User Viking = new User(1, "Viking12", "lösenord", "Viking", "Hesse", 18, true);
+            
+            User Kian = new User(3, "Kian", "123", "Kian", "Loniakan", 18, true);
+
+            User Liam = new User(4, "Liam", "daddy123", "Liam", "Turelid", 18, true);
+
+
+        }
+    }
+}
